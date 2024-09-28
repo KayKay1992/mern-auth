a153dea495d85a1917e2462cdaa22fc13b728aa7
KayKay1992 <kennethnwankpa1992@gmail.com>
2024-09-28 08:21:40 -0700

    Create Sign in API Route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
-import User from "../model/user.model.js";
+import jwt from "jsonwebtoken";
 import bcryptjs from "bcryptjs"
+import User from "../model/user.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const signUp = async (req, res, next) => {
     // Add user to the database
@@ -15,4 +17,27 @@ export const signUp = async (req, res, next) => {
     }
     
     
+}
+
+export const signIn = async (req, res, next) => {
+    // Authenticate user using the provided credentials
+    const { email, password } = req.body
+
+    //finding a valid email and password
+    try {
+    const validUser = await User.findOne({ email });
+    if(!validUser) 
+        return next(errorHandler (404, "User not found"));
+        const validPassword = bcryptjs.compare(password, validUser.password);
+        if(!validPassword) return next(errorHandler (404, "Wrong Credentials"));
+        
+        //Generate and send a JWT token
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+        const {password: hashedPassword, ...rest} = validUser._doc;
+        const expiryDate = new Date(Date.now() + 3600000)
+        //Send the token in the response with a cookie
+        res.cookie('access_token', token, {httpOnly: true, expires: expiryDate}).status(200).json(rest);
+    } catch (error) {
+        return next(error);
+    }
 }
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import {signUp} from '../controllers/auth.controller.js';
+import { signIn } from '../controllers/auth.controller.js';
 
 
 const router = express.Router()
 
 router.post('/signUp', signUp)
+router.post('/signin', signIn)
 
 export default router;
