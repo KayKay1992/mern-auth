6821cad29897d2e414974289d077e0f80a59e151
KayKay1992 <kennethnwankpa1992@gmail.com>
2024-09-30 08:30:57 -0700

    Adding Redux Persist : Preserves your data even after refreshing your page.

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,16 +1,27 @@
-import {configureStore} from '@reduxjs/toolkit'
+import { combineReducers, configureStore} from '@reduxjs/toolkit'
+import {persistReducer} from  'redux-persist'
+import storage from 'redux-persist/lib/storage';
 // Import your reducers
 import userReducer from './user/userSlice.js';
+import persistStore from 'redux-persist/es/persistStore';
+
+const rootReducer =combineReducers({user: userReducer})
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+ 
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
    // Add your reducers here
-    reducer: {
-        // user: userReducer,
-        user: userReducer,
-        // // Add your other reducers here
-    }, 
+    reducer: persistedReducer, 
      // Add your middleware here
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({
        serializableCheck: false,
     }),
 })
+
+export const persistor = persistStore(store);
