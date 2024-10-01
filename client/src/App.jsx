4ce978932b1f883a0aea936f68d71b6115a60f90
KayKay1992 <kennethnwankpa1992@gmail.com>
2024-10-01 07:55:50 -0700

    Update Header Component and made profile page private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,11 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-
 import SignIn from './pages/SignIn'
 import About from './pages/About'
 import SignUp from './pages/SignUp'
 import Profile from './pages/Profile'
 import Home from './pages/Home'
-import Header from './component/header'
+import Header from './component/Header'
+import PrivateRoute from './component/privateRoute'
 
 export default function App() {
   return (
@@ -15,8 +15,11 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/About" element={<About />} />
         <Route path="/SignIn" element={<SignIn />} />  
-        <Route path="/SignUp" element={<SignUp />} />  
-        <Route path="/Profile" element={<Profile/>} />  
+        <Route path="/SignUp" element={<SignUp />} /> 
+        <Route element={<PrivateRoute />}>
+        <Route path="/Profile" element={<Profile/>} /> 
+        </Route>
+
       </Routes>
     </BrowserRouter>
   )
--- client/src/component/Header.jsx
+++ client/src/component/Header.jsx
@@ -1,8 +1,9 @@
 import { Link } from "react-router-dom"
+import { useSelector } from 'react-redux'
 
-Link
 
 export default function Header() {
+   const {currentUser} = useSelector((state) => state.user)  
   return (
     <div className='bg-slate-200'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3 '>
@@ -17,8 +18,11 @@ export default function Header() {
                <Link to='/About'>
                     <li>About</li>
                </Link>
-               <Link to='/Signin'>
-                    <li>Sign-In</li>
+               <Link to='/profile'>
+                    { currentUser ? (
+                         <img src={currentUser.profilePicture} alt="profile" className='h-7 w-7 rounded-full object-cover'/>
+                    ) : (  <li>Sign-In</li>)  
+                    } 
                </Link>
             </ul>
         </div>
